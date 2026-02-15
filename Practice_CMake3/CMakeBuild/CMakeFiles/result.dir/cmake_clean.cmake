6287f0cd5c81957a0ea2f3ed541a757b8e46823d
KarismMarisam <162860527+KarismMarisam@users.noreply.github.com>
2026-02-15 18:55:54 +0800

    Add files via upload

---
+++ Practice_CMake3/CMakeBuild/CMakeFiles/result.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/result.dir/link.d"
+  "/home/m0nisa/CmakePractice/Practice_CMake3/result"
+  "/home/m0nisa/CmakePractice/Practice_CMake3/result.pdb"
+  "CMakeFiles/result.dir/main.cpp.o"
+  "CMakeFiles/result.dir/main.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/result.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Practice_CMake3/main.cpp
@@ -0,0 +1,5 @@
+#include <stdio.h>
+#include "Libraries/cal.h"//using other's libraries needs the "xx.h" file,you cant see how functions works
+int main(){
+     printf("%d",mul(5,2));
+}
